e5f5190872ef4e1a3a1920d4634ef0f6d370d9a7
nikhiluppala <uppala.n@northeastern.edu>
2024-04-16 10:10:49 -0400

    Working on Carousel

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Navigation from './customer/components/Navigation/Navigation.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation />
+      <div>
+        Home
+      </div>
+    </div>
+  );
+}
+
+export default App;
