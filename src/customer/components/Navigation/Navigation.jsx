f8b1154be92ab9a9fa9017df697ca09d5868e746
nikhiluppala <uppala.n@northeastern.edu>
2024-04-17 11:41:59 -0400

    working on Carousel

--- src/customer/components/Navigation/Navigation.jsx
+++ src/customer/components/Navigation/Navigation.jsx
@@ -457,7 +457,7 @@ function Navigation() {
                       className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                       aria-hidden="true"
                     />
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">2</span>
                     <span className="sr-only">items in cart, view bag</span>
                   </a>
                 </div>
